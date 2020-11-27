16a340a9397cf4cd2a116c4542b5cab0e8b01cff
Alak Kanti Dutta”git config --global user.email alakd1985@gmail.com” <alakd1985@gmail.com>
2020-11-26 20:48:30 -0500

    Flow Lu CRM page is added

--- src/test/java/testcasesReallyCRM/AddAccountTestRCRM.java
+++ src/test/java/testcasesReallyCRM/AddAccountTestRCRM.java
@@ -11,13 +11,14 @@ import pagesReallyCRM.AddAccountpage;
 import pagesReallyCRM.ReallyCRMHomePage;
 
 public class AddAccountTestRCRM {
+	Random rand = new Random();
 
 	@Test(dataProviderClass = DataUtil.class, dataProvider = "dp")
 	public void addaccountTestRCRM(Hashtable<String, String> data) {
 		ReallyCRMHomePage rehomepage = new ReallyCRMHomePage();
 		AccountsHomePage aHomepage = rehomepage.gotoAcocuntsHomePage();
 		AddAccountpage addAccount = aHomepage.gotoAddAccountpage();
-		addAccount.createAccount(data.get("name") + new Random(), data.get("address"), data.get("zip"),
+		addAccount.createAccount(data.get("name") + rand.nextInt(), data.get("address"), data.get("zip"),
 				data.get("city"), data.get("state"), data.get("website"), data.get("phone"), data.get("typed"),
 				data.get("sectord"));
 
--- src/test/java/testcasesReallyCRM/OpportunityTestRCRM.java
+++ src/test/java/testcasesReallyCRM/OpportunityTestRCRM.java
@@ -11,12 +11,14 @@ import pagesReallyCRM.OpportunitiesHomePage;
 import pagesReallyCRM.ReallyCRMHomePage;
 
 public class OpportunityTestRCRM {
+	Random rand = new Random();
+
 	@Test(dataProviderClass = DataUtil.class, dataProvider = "dp")
 	public void opportunityTestRCRM(Hashtable<String, String> data) {
 		ReallyCRMHomePage rehomepage = new ReallyCRMHomePage();
 		OpportunitiesHomePage oppHomePage = rehomepage.gotoOpportunities();
 		CreateOpportunityHomePage createOppHome = oppHomePage.gotoAddOpportunities();
-		createOppHome.newOpportunity(data.get("oppName") + new Random(), data.get("nxtStep"), data.get("competitor"),
+		createOppHome.newOpportunity(data.get("oppName") + rand.nextInt(), data.get("nxtStep"), data.get("competitor"),
 				data.get("acname"));
 	}
 }
